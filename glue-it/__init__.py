77026bcc4151b9efdf7eee0bc4f9977da9248899
Youngjin Jo <yjinjo@berkeley.edu>
2022-10-20 14:10:09 +0900

    Add Flask Request Hook and Application Context

--- glue-it/__init__.py
+++ glue-it/__init__.py
@@ -2,21 +2,37 @@
 
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
     
     @app.route('/')
     def index():
-        return 'Hello World!'
-
-    from flask import jsonify, redirect, url_for
+        app.logger.info('RUN HELLOWORLD')
+        return 'hello world'
     
-    @app.route('/test/path/<path:subpath>')
-    def path(subpath):
-        return subpath
+    ''' Request Hook '''
+    from flask import g, current_app
     
-    @app.route('/test/urlfor/<path:subpath>')
-    def urlfor(subpath):
-        return redirect(url_for('path', subpath=subpath))
+    @app.before_first_request
+    def before_first_request():
+        app.logger.info('BEFORE_FIRST_REQUEST')
+    
+    @app.before_request
+    def before_request():
+        g.test=True
+        app.logger.info('BEFORE_REQUEST')
+        
+    @app.after_request
+    def after_request(response):
+        app.logger.info(f'g.test: {g.test}')
+        # app.logger.info(f'current_app.config: {current_app.config}')
+        app.logger.info('AFTER_REQUEST')
+        
+        return response
+    
+    @app.teardown_request
+    def teardown_request(exception):
+        app.logger.info('TEARDOWN_REQUEST')
     
     return app
