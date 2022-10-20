31b3a61569e4308674b5de994a20693f09d80f7d
Youngjin Jo <yjinjo@berkeley.edu>
2022-10-20 14:56:37 +0900

    Add Template Engine

--- glue-it/__init__.py
+++ glue-it/__init__.py
@@ -1,38 +1,21 @@
 # quick-memo/glue-it/__init__.py
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
     app = Flask(__name__)
     
+    if app.config['DEBUG']:
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+    
     @app.route('/')
     def index():
-        app.logger.info('RUN HELLOWORLD')
-        return 'hello world'
+        return render_template('index.html')
     
-    ''' Request Hook '''
-    from flask import g, current_app
+    @app.errorhandler(404)
+    def page_404(error):
+        return render_template('/404.html'), 404
     
-    @app.before_first_request
-    def before_first_request():
-        app.logger.info('BEFORE_FIRST_REQUEST')
-    
-    @app.before_request
-    def before_request():
-        g.test=True
-        app.logger.info('BEFORE_REQUEST')
-        
-    @app.after_request
-    def after_request(response):
-        app.logger.info(f'g.test: {g.test}')
-        # app.logger.info(f'current_app.config: {current_app.config}')
-        app.logger.info('AFTER_REQUEST')
-        
-        return response
-    
-    @app.teardown_request
-    def teardown_request(exception):
-        app.logger.info('TEARDOWN_REQUEST')
-    
+
     return app
